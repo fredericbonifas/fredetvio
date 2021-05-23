8227cb42f01204f1a1a543266668b0d72f27902c
Frederic Bonifas <fredericbonifas@gmail.com>
2021-05-23 19:35:55 +0200

    img size

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -96,7 +96,7 @@ export const pageQuery = graphql`
           blurbs {
             image {
               childImageSharp {
-                fluid(maxWidth: 240, quality: 64) {
+                fluid(maxWidth: 800, quality: 64) {
                   ...GatsbyImageSharpFluid
                 }
               }
